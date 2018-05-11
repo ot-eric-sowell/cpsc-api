282c5c9500c3faae61b1d683457f9d683a8e88a2
Eric Sowell <esowell@onetechnologies.net>
2018-05-11 07:25:27 -0500

    added new api endpoints

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const rawFile = fs.readFileSync(__dirname + '/data.csv', 'utf-8');
 const lines = rawFile.split('\n');
 const violations = [];
+const infractionsByViolation = {}
 
+let violationGroupId = 1;
 lines.forEach((line, i) => {
   const isHeaderLine = i === 0;
   if (isHeaderLine || line.length === 0) {
@@ -13,12 +15,26 @@ lines.forEach((line, i) => {
   }
 
   const linePieces = line.split(',');
-  violations.push({
+  const infraction = {
     date: linePieces[0],
     product: linePieces[1],
     violation: linePieces[2],
     firm: linePieces[3]
-  });
+  }
+
+  violations.push(infraction);
+
+  if (infractionsByViolation[infraction.violation] === undefined) {
+    infractionsByViolation[infraction.violation] = {
+      id: violationGroupId,
+      violation: infraction.violation,
+      infractions: [infraction]
+    };
+    violationGroupId = violationGroupId + 1;
+  }
+  else {
+    infractionsByViolation[infraction.violation].infractions.push(infraction);
+  }
 });
 
 var allowCrossDomain = function(req, res, next) {
@@ -44,9 +60,86 @@ app.get('/', (req, res) => {
     {
       url: '/api?skip=10&take=5',
       description: 'Skip 10 violations and return the next 5.'
+    },
+    {
+      url: '/api/violations',
+      description: 'Return a list of all the violations'
+    },
+    {
+      url: '/api/violation/:id',
+      description: 'Return all infractions of a specific violation'
+    },
+    {
+      url: '/api/search?q={query}',
+      description: 'Runs a search using the query to check product, firm, and violation. Supports skip and take.'
     }
   ]);
+});
+
+app.get('/api/search', (req, res) => {
+  let output = violations.slice();
+
+  if (req.query.q) {
+    const query = req.query.q.toLowerCase()
+    output = output.filter((x) => {
+      return x.product.toLowerCase().indexOf(query) > -1 ||
+        x.firm.toLowerCase().indexOf(query) > -1 ||
+        x.violation.toLowerCase().indexOf(query) > -1
+    });
+  }
+
+  const total = output.length;
+
+  if (req.query.skip) {
+    output.splice(0, req.query.skip)
+  }
+
+  if (req.query.take) {
+    output = output.splice(0, req.query.take)
+  }
+
+  res.send({
+    total: total,
+    infractions: output
+  })
 })
+
+app.get('/api/violation/:id', (req, res) => {
+  let set;
+  for (let key in infractionsByViolation) {
+    if (infractionsByViolation[key].id == req.params.id) {
+      set = infractionsByViolation[key];
+      break;
+    }
+  }
+
+  if (set === undefined) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send({
+    count: set.infractions.length,
+    id: set.id,
+    violation: set.violation,
+    infractions: set.infractions
+  })
+});
+
+app.get('/api/violations', (req, res) => {
+  const list = [];
+  for (let key in infractionsByViolation) {
+    list.push({
+      violation: key,
+      id: infractionsByViolation[key].id,
+      count: infractionsByViolation[key].infractions.length
+    })
+  }
+  res.send({
+    count: list.length,
+    violations: list
+  });
+});
 
 app.get('/api', (req, res) => {
 
